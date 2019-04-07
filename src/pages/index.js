3f181653930ff76978c1507cd87a6ba99e12d0bd
unknown <ikymetaverse@gmail.com>
2019-04-07 13:31:29 +0300

    Added button classes, roboto slab font, button examples in layout index for the moment

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,18 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+
+<div class="container">
+<button class="btn yellow">Yellow</button>
+    <button class="btn gray">gray</button>
+    <button class="btn red">red</button>
+    <button class="btn purple">purple</button>
+    <button class="btn">default</button>
+    <button class="btn red" disabled>red disabled</button>
+</div>
+
     <Button buttonTitle={"Sticker"} />
+
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
