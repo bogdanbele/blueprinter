d666abda66752b5a25549183c509d09fa0fe2e80
Bogdan Bele <>
2019-07-02 14:03:28 +0200

    Social Media Regex Works

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,36 @@
 import React from "react"
-import { Link } from "gatsby"
+import {Link} from "gatsby"
 
 import Layout from "../layouts/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 import Button from "../components/Button/button"
-import getSocialMediaType from "../scripts/social-media-type.js"
+import ReturnComponent from "../scripts/social-media-type.js"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]}/>
     <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something {getSocialMediaType("https://twitter.com/realDonaldTrump")}great.</p>
-    
+    <p>Welcome to your new Gatsby site
+      <ReturnComponent link={"https://www.instagram.com/albertosings/"}/></p>
+    <p>Now go build something great.</p>
 
-<div className="container">
-<button className="btn yellow">Yellow</button>
-    <button className="btn gray">gray</button>
-    <button className="btn red">red</button>
-    <button className="btn purple">purple</button>
-    <button className="btn">default</button>
-    <button className="btn red" disabled>red disabled test</button>
-</div>
+    <div className="container">
+      <Button className="btn yellow">Yellow</Button>
+      <button className="btn gray">gray</button>
+      <button className="btn red">red</button>
+      <button className="btn purple">purple</button>
+      <button className="btn">default</button>
+      <button className="btn red" disabled>red disabled test</button>
+    </div>
 
-    <Button buttonTitle={"Sticker"} />
+    <Button buttonTitle={"Sticker"}/>
 
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div style={{
+      maxWidth: `300px`,
+      marginBottom: `1.45rem`
+    }}>
+      <Image/>
     </div>
     <Link to="/page-2/">Go to page 2</Link>
   </Layout>
--- src/scripts/social-media-type.js
+++ src/scripts/social-media-type.js
@@ -1,21 +1,40 @@
-function getSocialMediaType(string ){
-    
-    const regExp = /.*\.?(twitter\.com)\/([A-z0-9_]+\/?)/gm
-    
-    let m;
+import React from "react"
+import {FaTwitter, FaInstagram} from "react-icons/fa"
+import {FaFacebook} from "react-icons/fa"
 
-    while ((m = regExp.exec(string)) !== null) {
-        // This is necessary to avoid infinite loops with zero-width matches
-        if (m.index === regExp.lastIndex) {
-            regExp.lastIndex++;
-        }
-        
-        // The result can be accessed through the `m`-variable.
-        m.forEach((match, groupIndex) => {
-            console.log(`Found match, group ${groupIndex}: ${match}`);
-        });
-    }
-
+function isTwitterLink(string) {
+  const regExp = /http(s)?:\/\/(.*\.)?twitter\.com\/[A-z0-9_]+\/?/gm
+  return regExp.test(string)
+}
+function isFacebookLink(string) {
+  const regExp = /http(s)?:\/\/(www\.)?(facebook|fb)\.com\/[A-z0-9_\-\.]+\/?/gm
+  return regExp.test(string)
 }
 
-export default getSocialMediaType
+function isInstagramLink(string) {
+  const regExp = /https?:\/\/(www\.)?instagram\.com\/([A-Za-z0-9_](?:(?:[A-Za-z0-9_]|(?:\.(?!\.))){0,28}(?:[A-Za-z0-9_]))?)/gm
+  return regExp.test(string)
+}
+
+function random(string) {
+
+  switch (true) {
+    case(isTwitterLink(string)):
+      {
+        return <FaTwitter/>
+      }
+    case(isFacebookLink(string)):
+      {
+        return <FaFacebook/>
+      }
+    case(isInstagramLink(string)):
+      {
+        return <FaInstagram/>
+      }
+    default:
+      return <FaFacebook/>
+  }
+}
+
+const ReturnComponent = ({link}) => (random(link))
+export default ReturnComponent
