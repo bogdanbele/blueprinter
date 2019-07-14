e3d593dbf02397d977e8f2a7b776790a56ed7cd3
Bogdan Bele <>
2019-07-14 16:02:42 +0200

    Initial implementation of contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,28 +1,19 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { Component } from "react";
+import { Link } from "gatsby";
 import Button from "../components/Button/button";
+import ContactForm from "../components/ContactForm/contact-form";
+import Layout from "../layouts/layout";
+import SEO from "../components/seo";
 
-import Layout from "../layouts/layout"
-import SEO from "../components/seo"
+class ContactPage extends Component {
+  render() {
+    return (
+      <Layout>
+        <SEO title="Page two" />
+        <ContactForm email="contact.philip.nilsson@gmail.com" />
+      </Layout>
+    );
+  }
+}
 
-const SecondPage = () => (
-  <Layout>
-    <SEO title="Page two" />
-
- <div id="ll-schedule-embed" data-schedule-id="591237"></div>
-<script type="text/javascript" src="https://scheduler.leaguelobster.com/static/embed_en.js"></script>
-
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
-
-         <div className="container">
-        <Button className="primary wide" buttonTitle="Contact">
-          primary
-        </Button>
-      </div>
-      
-  </Layout>
-)
-
-export default SecondPage
+export default ContactPage;
