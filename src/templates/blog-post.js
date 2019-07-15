a6c73d2cda6f26bc9012ccc60b5931310fda61b1
Bogdan Bele <>
2019-07-15 10:00:52 +0200

    BlogWidth Fix

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,21 +2,24 @@ import React from "react";
 import { Link } from "gatsby";
 import Layout from "../layouts/layout";
 import { graphql } from "gatsby";
+import Row from "../components/Row/row";
 
 export default function Template({ data }) {
   const post = data.markdownRemark;
 
   return (
     <Layout>
-      <div>
-        <Link to="/blog">Go Back</Link>
-        <hr />
-        <h1>{post.frontmatter.title}</h1>
-        <h4>
-          Posted by {post.frontmatter.author} on {post.frontmatter.date}
-        </h4>
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
-      </div>
+      <Row>
+        <div className="blog-page">
+          <Link to="/blog"> Go Back</Link>
+          <hr />
+          <h1>{post.frontmatter.title}</h1>
+          <h4>
+            Posted by {post.frontmatter.author} on {post.frontmatter.date}
+          </h4>
+          <div dangerouslySetInnerHTML={{ __html: post.html }} />
+        </div>
+      </Row>
     </Layout>
   );
 }
