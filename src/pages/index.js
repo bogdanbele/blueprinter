cd59dd4f6f6b2f37c134e198d107314e569e857b
Bogdan Bele <bogdan.bele.dev@gmail.com>
2019-09-30 18:40:12 +0200

    Remove local fonts and scoped scss fonts globally

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,29 +4,30 @@ import Layout from "../layouts/layout";
 import SEO from "../components/seo";
 import {FaGithub} from "react-icons/fa"
 import {Flex, Button, Icon, Row, Item} from "blueprint-components-react";
+import {navigate} from "@reach/router";
 
 
 const IndexPage = () => (
     <Layout>
         <SEO title="Home" keywords={[`gatsby`, `application`, `react`]}/>
         <Row>
-            <h1>Hello people</h1>
-            <p>Welcome to our new headquarters. This website is currently under construction
-                with no clear date of release. If you would like to stay up to date with the
-                current development process we highly suggest heading to the contact page.
+            <h1>Working hard</h1>
+            <p>
+                The ncweb team is working hard to bring you our official website online.
             </p>
-            <Icon className="primary round">
-                <FaGithub/>
-            </Icon>
+            <p>
+                If you would like to stay up to date with the current development process
+                we highly recommend heading to the contact page.
+            </p>
             <Button onClick={
-                () => console.log('bean')
-            }>Click</Button>
+                () => navigate("/contact/")
+            }>Contact</Button>
         </Row>
         <Row className="centered full">
             <Flex className="flex--2 shadow">
                 <Item>
                     <div>
-                        <h1>This website is my official digital visit card. </h1>
+                        <h2>This website is my official digital visit card. </h2>
                         <p>
                             I’m a 28 years old ambitious and extremely passionate IT and Communication graduate
                             with a great interest in UX-Research, Qualitative and Quantitative methods and Human
