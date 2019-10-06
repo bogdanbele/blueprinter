149c287a128d53091895c84271c7979e024f3bb1
Bogdan Bele <bogdan.bele.dev@gmail.com>
2019-10-06 20:30:13 +0200

    Fixed typo

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
         name: `images`,
         path: `${__dirname}/src/images`,
       }
-    }
+    },
     "gatsby-transformer-json",
     {
       resolve: `gatsby-source-filesystem`,
