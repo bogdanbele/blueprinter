53f2f88c71b2e8532cf29ccf964869f0f6edf7d7
Bogdan Bele <bogdan.bele.dev@gmail.com>
2019-10-07 14:53:36 +0200

    Fixing fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,52 +1,60 @@
 module.exports = {
-  siteMetadata: {
-    title: `ncweb`,
-    description: `Before Steve Jobs was the real Big Mac.`,
-    author: `@bogdanbele`,
-  },
-  plugins: [
-    `gatsby-plugin-catch-links`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-sass`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      }
+    siteMetadata: {
+        title: `ncweb`,
+        description: `Before Steve Jobs was the real Big Mac.`,
+        author: `@bogdanbele`,
     },
-    "gatsby-transformer-json",
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `data`,
-        path: `${__dirname}/src/config/`,
-      }
-    },
-    `gatsby-transformer-sharp`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `pages`,
-        path: `${__dirname}/src/blog/`,
-      }
-    },
-    `gatsby-transformer-remark`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#0096ff`,
-        theme_color: `#0096ff`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // 'gatsby-plugin-offline',
-  ],
+    plugins: [
+        `gatsby-plugin-catch-links`,
+        `gatsby-plugin-react-helmet`,
+        `gatsby-plugin-sass`,
+        {
+            resolve: 'gatsby-plugin-web-font-loader',
+            options: {
+                google: {
+                    families: ['Quicksand', 'Roboto']
+                }
+            }
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `images`,
+                path: `${__dirname}/src/images`,
+            }
+        },
+        "gatsby-transformer-json",
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `data`,
+                path: `${__dirname}/src/config/`,
+            }
+        },
+        `gatsby-transformer-sharp`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `pages`,
+                path: `${__dirname}/src/blog/`,
+            }
+        },
+        `gatsby-transformer-remark`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `gatsby-starter-default`,
+                short_name: `starter`,
+                start_url: `/`,
+                background_color: `#0096ff`,
+                theme_color: `#0096ff`,
+                display: `minimal-ui`,
+                icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+            },
+        },
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.dev/offline
+        // 'gatsby-plugin-offline',
+    ],
 }
