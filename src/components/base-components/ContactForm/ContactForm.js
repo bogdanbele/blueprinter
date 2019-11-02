b0d8740757053bebb5731dea092ea0f931a39a93
Bogdan Bele <bogdan.bele.dev@gmail.com>
2019-11-02 18:18:42 +0100

    New Contact Form

--- src/components/base-components/ContactForm/ContactForm.js
+++ src/components/base-components/ContactForm/ContactForm.js
@@ -15,18 +15,34 @@ function encode(data) {
 export default class ContactForm extends React.Component {
 
     state = {
-        firstName: '',
-        lastName: '',
-        email: '',
-        message: ''
+        values: {
+            firstName: '',
+            lastName: '',
+            email: '',
+            message: ''
+        },
+        isValid: {
+            firstName: false,
+            lastName: false,
+            email: false,
+            message: false
+        },
+        validation: {
+            firstName: "[a-zA-Z]{2,20}",
+            lastName: "[a-zA-Z]{2,20}",
+            email: "[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{1,63}$",
+            message: "[a-zA-Z]{20,300}"
+        }
     };
+
 
     handleSubmit = e => {
         const form = e.target;
+        console.log(form)
         fetch("/", {
             method: "POST",
             headers: {"Content-Type": "application/x-www-form-urlencoded"},
-            body: encode({"form-name": form.getAttribute('name'), ...this.state})
+            body: encode({"form-name": form.getAttribute('name'), ...this.state.values})
         })
             .then(() => navigate("/"))
             .catch(error => alert(error));
@@ -39,39 +55,35 @@ export default class ContactForm extends React.Component {
         const value = target.value;
         const name = target.name;
 
-        const pattern = RegExp(event.target.pattern);
-
         this.setState({
-            [name]: value,
+            values: {...this.state.values, [name]: value}
         });
 
-        if ((pattern).test(value)) {
-            target.setCustomValidity('');
-            return
-        }
+        this.setState({
+            isValid: {...this.state.isValid, [name]: (RegExp(this.state.validation[name]).test(value))}
+        });
 
-        switch (name) {
-            case "firstName": {
-                target.setCustomValidity("Your first name should contain at least two characters");
-                break;
-            }
-            case "lastName": {
-                target.setCustomValidity("Your last name should contain at least two characters")
-                break;
-            }
-            case "email": {
-                target.setCustomValidity("Please enter a valid email")
-                break;
-            }
-            case "message": {
-                target.setCustomValidity("Your message has to be at least 20 characters long.")
-                break;
+        console.log(this.handleFormValidation())
+    };
+
+    handleValidation = name => {
+        return !(this.state.values[name] === '' || (RegExp(this.state.validation[name]).test(this.state.values[name])))
+    };
+
+    handleFormValidation = () => {
+        for (let key in this.state.isValid) {
+            if (this.state.isValid.hasOwnProperty(key)) {
+                if (this.state.isValid[key] === false) {
+                    return false
+                }
             }
         }
+        return true
     };
 
 
     render() {
+
         const {className} = this.props;
         return (
             <form
@@ -86,59 +98,60 @@ export default class ContactForm extends React.Component {
                 <input type="hidden" name="form-name" value="contact"/>
                 <p hidden>
                     <label>
-                        Don’t fill this out: <input name="bot-field" onChange={this.handleChange}/>
+                        Don’t fill this out: <input name="bot-field" onChange={this.handleInputChange}/>
                     </label>
                 </p>
                 <WhiteInputField
-                    inputProps={{
-                        required: true,
-                        pattern: "[a-zA-Z]{2,20}",
-                    }}
+                    required={true}
+                    variant="outlined"
                     name="firstName"
                     label="first name"
+                    error={this.handleValidation('firstName')}
+                    helperText={this.handleValidation('firstName') ? "Your first name must be between 2 and 20 characters long" : ''}
                     onChange={this.handleInputChange}
                     margin="normal"
-                    value={this.state.firstName}
+                    value={this.state.values.firstName || ''}
                 />
                 <WhiteInputField
-                    inputProps={{
-                        required: true,
-                        pattern: "[a-zA-Z]{2,20}",
-                    }}
+                    required={true}
+                    variant="outlined"
+                    error={this.handleValidation('lastName')}
                     name="lastName"
                     label="last name"
+                    helperText={this.handleValidation('lastName') ? "Your last name must be between 2 and 20 characters long" : ''}
                     onChange={this.handleInputChange}
                     margin="normal"
-                    value={this.state.lastName}
+                    value={this.state.values.lastName || ''}
                 />
                 <WhiteInputField
-                    inputProps={{
-                        required: true,
-                        pattern: "[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{1,63}$"
-                    }}
+                    required={true}
+                    variant="outlined"
+                    error={this.handleValidation('email')}
+                    helperText={this.handleValidation('email') ? "You must input a valid email" : ''}
                     name="email"
                     label="email"
                     onChange={this.handleInputChange}
                     margin="normal"
-                    value={this.state.email}
+                    value={this.state.values.email || ''}
                 />
                 <div className='form-group'>
                     <WhiteInputField
-                        inputProps={{
-                            required: true,
-                            pattern: "[a-zA-Z]{20,300}",
-                        }}
+                        required={true}
+                        variant="outlined"
                         aria-label="minimum height"
+                        helperText={this.handleValidation('message') ? "Your message must be between 20 and 300 characters" : ''}
                         multiline={true}
+                        rows={5}
                         rowsMax={10}
                         name="message"
                         onChange={this.handleInputChange}
-                        value={this.state.message}
+                        value={this.state.values.message}
                         placeholder="Write us a message ( up to 300 characters )"/>
                 </div>
                 <div className='form-group'>
 
                     <Button
+                        disabled={!this.handleFormValidation()}
                         type="submit"
                         className="button Button--wide"
                     >Send</Button>
@@ -148,35 +161,57 @@ export default class ContactForm extends React.Component {
     }
 };
 
-ContactForm.propTypes = {
+ContactForm
+    .propTypes = {
     className: PropTypes.string
 };
 
 
-const WhiteInputField = withStyles({
-    root: {
-        // Underline on Focus
-        '& .MuiInput-underline:after': {
-            borderColor: 'white'
-        },
+const
+    WhiteInputField = withStyles({
+        root: {
+            // Underline on Focus
+            '& .MuiInput-underline:after': {
+                borderColor: 'white'
+            },
 
-        '& .MuiInputBase-input': {
-            color: 'white'
-        },
+            '& .MuiOutlinedInput-notchedOutline': {
+                color: 'white',
+                borderColor: 'gray'
+            },
 
-        // Default Underline
-        '& .MuiInput-underline:before': {
-            borderColor: 'gray',
-        },
+            '& .MuiOutlinedInput-notchedOutline:hover': {
+                color: 'white',
+                borderColor: 'white'
+            },
 
-        // Default Underline Hover
-        '& .MuiInput-root:hover:not($disabled):before': {
-            borderBottom: '2px solid white'
-        },
+            '& .MuiOutlinedInput-root:hover': {
+                color: 'white',
+                borderColor: 'white'
+            },
 
-        // Text Color
-        '& .MuiFormLabel-root': {
-            color: 'white',
+            '& .MuiInputBase-input': {
+                color: 'white'
+            },
+
+            // Default Underline
+            '& .MuiInput-underline:before': {
+                borderColor: 'gray',
+            },
+
+            // Default Underline
+            '& .MuiFormHelperText-root': {
+                color: 'red',
+            },
+
+            // Default Underline Hover
+            '& .MuiInput-root:hover:not($disabled):before': {
+                borderBottom: '2px solid white'
+            },
+
+            // Text Color
+            '& .MuiFormLabel-root': {
+                color: 'white',
+            }
         }
-    }
-})(TextField);
+    })(TextField);
