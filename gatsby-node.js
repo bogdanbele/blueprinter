376af99a62c5c580d553f317c822e6f4dec9eeb1
Bogdan Bele <bogdan.bele.dev@gmail.com>
2019-12-10 21:20:06 +0100

    Scrolling Fix

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,42 +1,3 @@
-const path = require('path');
-/**
-exports.createPages = ({actions, graphql}) => {
-	const {createPage} = actions;
-
-	const postTemplate = path.resolve('src/components/template-components/blog-post.js');
-
-	return graphql(`
-    {
-      allMarkdownRemark {
-        edges {
-          node {
-            html
-            id
-            frontmatter {
-              path
-              title
-              date
-              author
-            }
-          }
-        }
-      }
-    }
-  `).then(res => {
-		if (res.errors) {
-			return Promise.reject(res.errors);
-		}
-
-		res.data.allMarkdownRemark.edges.forEach(({node}) => {
-			createPage({
-				// Set to node.frontmatter.path if relevant. CUrrently no longer needed
-				path: "/",
-				component: postTemplate,
-			});
-		});
-	});
-};
-
 exports.onCreateWebpackConfig = ({stage, loaders, actions}) => {
 	if (stage === 'build-html') {
 		actions.setWebpackConfig({
@@ -56,4 +17,3 @@ exports.onCreateWebpackConfig = ({stage, loaders, actions}) => {
 		);
 	}
 };
- */
