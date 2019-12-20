40150bd01bf07f91ecb95701b98a0c78d3e3dd2a
Bogdan Bele <bogdan.bele@yahoo.com>
2019-12-20 15:37:58 +0100

    DropDown with Features

--- src/pages/plans/order.js
+++ src/pages/plans/order.js
@@ -6,31 +6,80 @@ import PageHeader from '../../components/template-components/PageHeader';
 import Row from '../../components/base-components/Row';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
+import Input from '@material-ui/core/Input';
+import Checkbox from '@material-ui/core/Checkbox';
+import ListItemText from '@material-ui/core/ListItemText';
 
 const OrderPage = ({ data }) => {
 	const orderArray = window.history.state !== null ? window.history.state.order : [];
 	const allPlans = data.allContentfulPlanFeature.edges;
-	const [missingPlans, setMissingPlans] = useState([]);
+	let missingPlans = null;
+
+	const [chosenPlans, setChosenPlans] = useState([]);
 
 	const page = data.allContentfulPage.edges[0].node;
 
+	const handleChange = event => {
+		setChosenPlans(event.target.value);
+	};
+
+	const ITEM_HEIGHT = 48;
+	const ITEM_PADDING_TOP = 8;
+	const MenuProps = {
+		PaperProps: {
+			style: {
+				maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+                width: 250,
+			},
+		},
+	};
+
+	function returnList() {
+		return (
+			<Select
+				labelId="demo-mutiple-checkbox-label"
+				id="demo-mutiple-checkbox"
+				style={{ color: 'charcoal', backgroundColor : 'aliceblue' }}
+                multiple
+                labelWidth
+                variant={'outlined'}
+				value={chosenPlans}
+				onChange={handleChange}
+				input={<Input className='px-4'/>}
+				renderValue={selected => selected.join(', ')}
+				MenuProps={MenuProps}
+			>
+				{console.log(missingPlans)}
+				{missingPlans.map(name => {
+					console.log(name);
+					return (
+						<MenuItem key={name.title} value={name.title}>
+							<Checkbox checked={chosenPlans.indexOf(name.title) > -1} />
+							<ListItemText primary={name.title} />
+						</MenuItem>
+					);
+				})}
+			</Select>
+		);
+	}
+
 	useEffect(() => {
 		if (window.history.state) {
-			let selectedPlansFilter = orderArray.map(plan => {
-				return plan.id;
-			});
-
-			let filteredPlan = allPlans
-				.filter(plan => !selectedPlansFilter.includes(plan.node.id))
-				.map(plan => {
-					return { title: plan.node.title, excerpt: plan.node.excerpt.excerpt };
-				});
-			missingPlans.length === 0 ? setMissingPlans(filteredPlan) : null;
 			console.log(missingPlans);
 		} else {
 			console.log('wrong way buddy');
 		}
 	});
+
+	let selectedPlansFilter = orderArray.map(plan => {
+		return plan.id;
+	});
+
+	missingPlans = allPlans
+		.filter(plan => !selectedPlansFilter.includes(plan.node.id))
+		.map(plan => {
+			return { title: plan.node.title, excerpt: plan.node.excerpt.excerpt };
+		});
 
 	return (
 		<Layout>
@@ -41,8 +90,10 @@ const OrderPage = ({ data }) => {
 				isHeaderVisible={page.isHeaderVisible}
 				isHeaderTextVisible={page.isHeaderTextVisible}
 			/>
-			<Row className="around">
-				<h1>test</h1>
+			{console.log(missingPlans)}
+			<Row className="around column">
+				<h2>Select some extra features</h2>
+				{returnList()}
 			</Row>
 		</Layout>
 	);
