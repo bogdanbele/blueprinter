4f28cfd6c954f58f966168a1433c18f11a928862
Bogdan Bele <bogdan.bele@yahoo.com>
2019-12-21 18:27:03 +0100

    Form

--- src/pages/plans/order.js
+++ src/pages/plans/order.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { graphql, Link, StaticQuery } from 'gatsby';
+import React, {useEffect, useState} from 'react';
+import {graphql, Link, StaticQuery} from 'gatsby';
 import Layout from '../../components/layout-components/layouts/layout';
 import SEO from '../../components/base-components/seo';
 import PageHeader from '../../components/template-components/PageHeader';
@@ -13,227 +13,232 @@ import {navigate} from "gatsby-link";
 import Button from "../../components/base-components/Button";
 import {withStyles} from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
+import styles from "../../components/layout-components/ContactForm/ContactForm.module.scss";
 
 function encode(data) {
-	return Object.keys(data)
-		.map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
-		.join('&');
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+        .join('&');
 }
 
 
-const OrderPage = ({ data }) => {
-	const [values, setValue] = useState({});
+const OrderPage = ({data}) => {
+    const [values, setValue] = useState({});
 
-	function handleSubmit(){
-		fetch('/', {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-			body: encode({ 'form-name': 'WebsiteQuote', ...values }),
-		})
-			.then(() => navigate('/'))
-			.catch(error => alert(error));
-	};
+    function handleSubmit() {
+        fetch('/', {
+            method: 'POST',
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            body: encode({'form-name': 'WebsiteQuote', ...values}),
+        })
+            .then(() => navigate('/'))
+            .catch(error => alert(error));
+    };
 
 
-	let orderArray = [];
-	let isInOrderFlow = [];
-	if (typeof window !== 'undefined') {
-		orderArray = window.history.state !== null ? window.history.state.order : [];
-		isInOrderFlow = window.history.state !== null ? true : false;
+    let orderArray = [];
+    let isInOrderFlow = [];
+    if (typeof window !== 'undefined') {
+        orderArray = window.history.state !== null ? window.history.state.order : [];
+        isInOrderFlow = window.history.state !== null ? true : false;
     }
 
-	const allPlans = data.allContentfulPlanFeature.edges;
+    const allPlans = data.allContentfulPlanFeature.edges;
 
-	let missingPlans = null;
+    let missingPlans = null;
 
-	const [chosenPlans, setChosenPlans] = useState([]);
+    const [chosenPlans, setChosenPlans] = useState([]);
 
-	const page = data.allContentfulPage.edges[0].node;
+    const page = data.allContentfulPage.edges[0].node;
 
-	const handleInputChange = event => {
-		const target = event.target;
-		const value = target.value;
-		console.log(target)
-		console.log(value)
+    const handleInputChange = event => {
+        const target = event.target;
+        const value = target.value;
+        console.log(target)
+        console.log(value)
 
-		const name = target.name;
+        const name = target.name;
 
-		setValue({...values, [name]:value})
-	};
+        setValue({...values, [name]: value})
+    };
 
-	const handleChange = event => {
-		setChosenPlans(event.target.value);
-		setValue({...values, ['extraPlans']:event.target.value});
-		console.log(event.target.value)
-	};
+    const handleChange = event => {
+        setChosenPlans(event.target.value);
+        setValue({...values, ['extraPlans']: event.target.value});
+        console.log(event.target.value)
+    };
 
-	const ITEM_HEIGHT = 48;
-	const ITEM_PADDING_TOP = 8;
-	const MenuProps = {
-		PaperProps: {
-			style: {
-				maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
-				width: 250,
-			},
-		},
-	};
+    const ITEM_HEIGHT = 48;
+    const ITEM_PADDING_TOP = 8;
+    const MenuProps = {
+        PaperProps: {
+            style: {
+                maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+                width: 250,
+            },
+        },
+    };
 
-	function consoleLog() {
-		console.log(values)
-		console.log(encode(values))
-	}
+    function consoleLog() {
+        console.log(values)
+        console.log(encode(values))
+    }
 
-	//TODO Add Fields for email, phone number option, and description ( comments ) optional.
+    //TODO Add Fields for email, phone number option, and description ( comments ) optional.
 
-	function returnList() {
-		return (
-			<>
-				<h2>Select some extra features</h2>
-				<Select
-					labelId="demo-mutiple-checkbox-label"
-					id="demo-mutiple-checkbox"
-					style={{ color: 'charcoal', backgroundColor: 'aliceblue' }}
-					multiple
-					variant={'outlined'}
-					value={chosenPlans}
-					onChange={handleChange}
-					input={<Input className="px-4" />}
-					renderValue={selected => selected.join(', ')}
-					MenuProps={MenuProps}
-				>
-					{console.log(missingPlans)}
-					{missingPlans.map(name => {
-						return (
-							<MenuItem key={name.title} value={name.title}>
-								<Checkbox checked={chosenPlans.indexOf(name.title) > -1} />
-								<ListItemText primary={name.title} />
-							</MenuItem>
-						);
-					})}
-				</Select>
-			</>
-		);
-	}
+    function returnList() {
+        return (
+            <>
+                <h2>Select some extra features</h2>
+                <Select
+                    labelId="demo-mutiple-checkbox-label"
+                    id="demo-mutiple-checkbox"
+                    style={{color: 'charcoal', backgroundColor: 'aliceblue'}}
+                    multiple
+                    variant={'outlined'}
+                    value={chosenPlans}
+                    onChange={handleChange}
+                    input={<Input className="px-4"/>}
+                    renderValue={selected => selected.join(', ')}
+                    MenuProps={MenuProps}
+                >
+                    {console.log(missingPlans)}
+                    {missingPlans.map(name => {
+                        return (
+                            <MenuItem key={name.title} value={name.title}>
+                                <Checkbox checked={chosenPlans.indexOf(name.title) > -1}/>
+                                <ListItemText primary={name.title}/>
+                            </MenuItem>
+                        );
+                    })}
+                </Select>
+            </>
+        );
+    }
 
-	useEffect(() => {
-		if (window.history.state) {
-			console.log(missingPlans);
-		} else {
-			console.log('wrong way buddy');
-		}
-	});
+    useEffect(() => {
+        if (window.history.state) {
+            console.log(missingPlans);
+        } else {
+            console.log('wrong way buddy');
+        }
+    });
 
-	let selectedPlansFilter = orderArray.map(plan => {
-		return plan.id;
-	});
+    let selectedPlansFilter = orderArray.map(plan => {
+        return plan.id;
+    });
 
-	missingPlans = allPlans
-		.filter(plan => !selectedPlansFilter.includes(plan.node.id))
-		.map(plan => {
-			return { title: plan.node.title, excerpt: plan.node.excerpt.excerpt };
-		});
+    missingPlans = allPlans
+        .filter(plan => !selectedPlansFilter.includes(plan.node.id))
+        .map(plan => {
+            return {title: plan.node.title, excerpt: plan.node.excerpt.excerpt};
+        });
 
-	console.log(isInOrderFlow);
-	return (
-		<Layout>
-			<SEO title="Order" />
-			<PageHeader
-				header={page.header}
-				headerText={page.headerText}
-				isHeaderVisible={page.isHeaderVisible}
-				isHeaderTextVisible={page.isHeaderTextVisible}
-			/>
-			<Row className="around column">
-				{isInOrderFlow ? returnList() : <p>Please follow to purchase flow</p>}
-			</Row>
-			<Row>
-				<ThemeInputStyle
-					required={true}
-					variant="outlined"
-					name="email"
-					label="Email"
-					onChange={handleInputChange}
-					margin="normal"
-					value={values.email || ''}
-				/>
-			</Row>
-			<Button onClick={consoleLog}>Click</Button>
-			<Button onClick={handleSubmit}>Click</Button>
+    console.log(isInOrderFlow);
+    return (
+        <Layout>
+            <SEO title="Order"/>
+            <PageHeader
+                header={page.header}
+                headerText={page.headerText}
+                isHeaderVisible={page.isHeaderVisible}
+                isHeaderTextVisible={page.isHeaderTextVisible}
+            />
+            <Row className="around column">
+                {isInOrderFlow ? returnList() : <p>Please follow to purchase flow</p>}
+            </Row>
+            <Row>
+                <form
+                    data-netlify="true"
+                    data-netlify-honeypot="bot-field"
+                >
+                    <ThemeInputStyle
+                        required={true}
+                        variant="outlined"
+                        name="email"
+                        label="Email"
+                        onChange={handleInputChange}
+                        margin="normal"
+                        value={values.email || ''}
+                    />
+                </form>
+            </Row>
+            <Button onClick={consoleLog}>Click</Button>
+            <Button onClick={handleSubmit}>Click</Button>
 
-		</Layout>
-	);
+        </Layout>
+    );
 };
 
 export const query = graphql`
-	{
-		allContentfulPage(filter: { title: { eq: "Order" } }) {
-			edges {
-				node {
-					title
-					headerText
-					header
-					isHeaderTextVisible
-					isHeaderVisible
-				}
-			}
-		}
-		allContentfulPlanFeature {
-			edges {
-				node {
-					id
-					title
-					excerpt {
-						excerpt
-						id
-					}
-				}
-			}
-		}
-	}
+    {
+        allContentfulPage(filter: { title: { eq: "Order" } }) {
+            edges {
+                node {
+                    title
+                    headerText
+                    header
+                    isHeaderTextVisible
+                    isHeaderVisible
+                }
+            }
+        }
+        allContentfulPlanFeature {
+            edges {
+                node {
+                    id
+                    title
+                    excerpt {
+                        excerpt
+                        id
+                    }
+                }
+            }
+        }
+    }
 `;
 
 
-
 const ThemeInputStyle = withStyles({
-	root: {
-		// Underline on Focus
-		'& .MuiInput-underline:after': {
-			borderColor: 'var(--color)',
-		},
+    root: {
+        // Underline on Focus
+        '& .MuiInput-underline:after': {
+            borderColor: 'var(--color)',
+        },
 
-		'& .MuiOutlinedInput-notchedOutline': {
-			color: 'var(--color)',
-			borderColor: 'gray',
-		},
+        '& .MuiOutlinedInput-notchedOutline': {
+            color: 'var(--color)',
+            borderColor: 'gray',
+        },
 
-		'& .MuiOutlinedInput-notchedOutline:hover': {
-			color: 'var(--color)',
-			borderColor: 'var(--color)',
-		},
+        '& .MuiOutlinedInput-notchedOutline:hover': {
+            color: 'var(--color)',
+            borderColor: 'var(--color)',
+        },
 
-		'& .MuiOutlinedInput-root:hover': {
-			color: 'var(--color)',
-			borderColor: 'var(--color)',
-		},
+        '& .MuiOutlinedInput-root:hover': {
+            color: 'var(--color)',
+            borderColor: 'var(--color)',
+        },
 
-		'& .MuiInputBase-input': {
-			color: 'var(--color)',
-		},
-		// Default Underline
-		'& .MuiInput-underline:before': {
-			borderColor: 'gray',
-		},
+        '& .MuiInputBase-input': {
+            color: 'var(--color)',
+        },
+        // Default Underline
+        '& .MuiInput-underline:before': {
+            borderColor: 'gray',
+        },
 
-		// Default Underline
-		'& .MuiFormHelperText-root': {
-			color: 'red',
-		},
+        // Default Underline
+        '& .MuiFormHelperText-root': {
+            color: 'red',
+        },
 
-		// Text Color
-		'& .MuiFormLabel-root': {
-			color: 'var(--color)',
-		},
-	},
+        // Text Color
+        '& .MuiFormLabel-root': {
+            color: 'var(--color)',
+        },
+    },
 })(TextField);
 
 export default OrderPage;
