92e2e635112890730d50d2b31b41f280de2107d0
Bogdan Bele <bogdan.bele@yahoo.com>
2019-12-27 22:39:40 +0100

    Project Wide Functional Components

--- src/components/template-components/TeamMember/TeamMember.js
+++ src/components/template-components/TeamMember/TeamMember.js
@@ -5,42 +5,40 @@ import styles from './TeamMember.module.scss';
 import Img from 'gatsby-image';
 import wrapWithParagraph from '../../../utils/helpers/TextWrapper';
 
-export default class TeamMember extends React.Component {
-	render() {
-	    const data = this.props.data;
+export default function TeamMember(props) {
+	const data = props.data;
 
-		const name = data.name;
-        const formattedDescription = wrapWithParagraph(data.description.description);
-        const skills = data.skills;
+	const name = data.name;
+	const formattedDescription = wrapWithParagraph(data.description.description);
+	const skills = data.skills;
 
-        const imageSrc = data.image.fixed;
-        const imageAlt = data.image.description;
+	const imageSrc = data.image.fixed;
+	const imageAlt = data.image.description;
 
-        // Alternate between image on the left and image on the right
-		let order = () =>
-			this.props.index % 2 !== 0 ? ' flex--row--column__reversed' : ' flex--row--column';
+	// Alternate between image on the left and image on the right
+	let order = () =>
+		props.index % 2 !== 0 ? ' flex--row--column__reversed' : ' flex--row--column';
 
-		return (
-			<Flex className="w-100 flex-column">
-				<h1>{name}</h1>
-				<Flex className={styles.TeamMember + order()}>
-					<Flex className="flex-column px-2">
-						{formattedDescription}
-						<br/>
-						<h2>Area of expertise:</h2>
-						<ul>
-							{skills.map((item, key) => (
-								<li key={key}>{item}</li>
-							))}
-						</ul>
-					</Flex>
-					<Flex className="flex--2 mx-auto">
-						<Img fixed={imageSrc} alt={imageAlt}/>
-					</Flex>
+	return (
+		<Flex className="w-100 flex-column">
+			<h1>{name}</h1>
+			<Flex className={styles.TeamMember + order()}>
+				<Flex className="flex-column px-2">
+					{formattedDescription}
+					<br/>
+					<h2>Area of expertise:</h2>
+					<ul>
+						{skills.map((item, key) => (
+							<li key={key}>{item}</li>
+						))}
+					</ul>
+				</Flex>
+				<Flex className="flex--2 mx-auto">
+					<Img fixed={imageSrc} alt={imageAlt}/>
 				</Flex>
 			</Flex>
-		);
-	}
+		</Flex>
+	);
 }
 
 TeamMember.propTypes = {
